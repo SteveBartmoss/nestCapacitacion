8e09ad541a9d054884000a3c4b94db4a8d909b79
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-07-04 09:22:10 -0600

    feat: Se agrego un metodo y getter, para ejemplificar el uso de estos en una clase

--- typescript-intro/src/bases/03-classes.ts
+++ typescript-intro/src/bases/03-classes.ts
@@ -4,11 +4,23 @@ export class Pokemon {
     public readonly id: number;
     public name: string;
 
-
     constructor(id: number, name: string){
         this.id = id;
         this.name = name;
     }
+
+    get imageUrl(): string{
+        return `https://pokemon.com/${this.id}.jpg`;
+    }
+
+    scream(){
+        console.log(`${this.name.toUpperCase()} !!!`);
+    }
+
+    speack(){
+        console.log(`${this.name}, ${this.name}`);
+    }
+    
 }
 
 //forma abrevida se debe tener inactiva la configuracion de erasableSyntaxOnly en config.ts

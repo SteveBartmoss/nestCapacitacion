ef32343d0f5bbc00d5b7403a0c3e5444fc9a96a0
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-07-05 15:34:18 -0600

    feat: Se implemento un servicio para manejar la logica del negocio de los automoviles

---
+++ car-dealership/src/cars/cars.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+
+    private cars = [
+        {
+            id: 1,
+            brand: 'Toyota',
+            model: 'Corolla'
+        },
+        {
+            id: 2,
+            brand: 'Honda',
+            model: 'Civic'
+        },
+        {
+            id: 3,
+            brand: 'Jeep',
+            model: 'Cherokee'
+        }
+    ];
+
+    public findAll(){
+        return this.cars
+    }
+
+    public findById(id: number){
+        return this.cars.filter(elemet => elemet.id == id)
+    }
+
+}

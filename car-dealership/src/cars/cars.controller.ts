8db68d815616649dc0f95ea87525c7cd722316c3
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-07-05 15:40:21 -0600

    feat: Se implemento la inyeccion de la dependencia del servicio de vehiculos

--- car-dealership/src/cars/cars.controller.ts
+++ car-dealership/src/cars/cars.controller.ts
@@ -1,17 +1,20 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
 
-    private cars = ['Toyota','Honda','Jeep'];
+    constructor(
+        private readonly carsService: CarsService
+    ){}
 
     @Get()
     getAllCars(){
-        return this.cars;
+        return this.carsService.findAll();
     }
 
     @Get(':id')
     getCarById( @Param('id') id){
-        return this.cars[parseInt(id)]
+        return this.carsService.findById(parseInt(id));
     }
 }

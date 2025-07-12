842db7171d1132400846c90c9dc68a49eea3be59
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-07-12 15:15:31 -0600

    feat: Se agrego la captuta de una excepcion al momento de insertar en base de datos

--- pokedex/src/pokemon/pokemon.service.ts
+++ pokedex/src/pokemon/pokemon.service.ts
@@ -1,11 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return createPokemonDto;
+
+  constructor(
+
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+
+  ){}
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
+
+    try{
+
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+    } catch (error){
+      console.log(error)
+
+      throw new BadRequestException(`code: ${error.code} keyValue: ${JSON.stringify(error.keyValue) } message: ${error.errorResponse.errmsg} `)
+    }
+
   }
 
   findAll() {

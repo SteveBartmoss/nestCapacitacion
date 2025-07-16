36f55fd5fdef096b1e7eb7f55326fdd9b216fc97
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-07-16 16:57:25 -0600

    Se inyecto la dependencia del adatador del axios

--- pokedex/src/seed/seed.service.ts
+++ pokedex/src/seed/seed.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { PokemonService } from 'src/pokemon/pokemon.service';
 import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
@@ -13,30 +13,45 @@ export class SeedService {
   constructor(
     private readonly pokemonService: PokemonService, //solucion og
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ){}
-
-  private readonly axios: AxiosInstance = axios;
 
   async executeSeed(){
 
+    await this.pokemonModel.deleteMany({})
+
     //let pokeCreate = new CreatePokemonDto()
 
-    const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
 
-    data.results.forEach(async (element)=>{
+    const pokemonToInsert: CreatePokemonDto[] = []
+
+    //const insertPromisesArray: Promise<any>[] = [];
+
+    data.results.forEach((element)=>{
 
       const segments = element.url.split('/');
       const no = +segments[segments.length-2]
 
-      await this.pokemonModel.create({name: element.name, no})
+      pokemonToInsert.push({name: element.name, no})
+
+      //await this.pokemonModel.create({name: element.name, no})
 
       //pokeCreate.name = element.name
       //pokeCreate.no = no
 
-      //this.pokemonService.create(pokeCreate)
+      /*
+      insertPromisesArray.push(
+        this.pokemonModel.create({name: element.name, no})
+      );
+      */
 
-    })
+    });
+
+    //await Promise.all(insertPromisesArray);
+
+    await this.pokemonModel.insertMany(pokemonToInsert);
 
     return 'Seed Executed';
 

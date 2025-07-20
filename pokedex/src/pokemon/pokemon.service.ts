c68b35cca2e31861867ce87603e40fa69e74b359
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-07-19 19:47:27 -0600

    feat: Se implemento el valor por defecto de la paginacion desde el archivo .env

--- pokedex/src/pokemon/pokemon.service.ts
+++ pokedex/src/pokemon/pokemon.service.ts
@@ -5,16 +5,22 @@ import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
 
+  private defaultLimit: number;
+
   constructor(
 
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly configService: ConfigService
 
-  ){}
+  ){
+    this.defaultLimit = configService.get<number>('defaultLimit') as number
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
@@ -33,7 +39,7 @@ export class PokemonService {
 
   async findAll(paginationDto: PaginationDto) {
 
-    const {limit = 10, offset = 0} = paginationDto;
+    const {limit = this.defaultLimit, offset = 0 } = paginationDto;
     
     return await this.pokemonModel.find().limit(limit).skip(offset).sort({no: 1}).select('-__v')
 

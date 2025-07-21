8c79bb292bedabbb7b1e5deb0593a7463cd050a5
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-07-21 13:22:22 -0600

    feat: se implemento una entidad para la creacion de un producto

--- teslo-shop/src/products/entities/product.entity.ts
+++ teslo-shop/src/products/entities/product.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Product {
@@ -10,5 +10,55 @@ export class Product {
         unique: true,
     })
     title: string;
-    
+
+    @Column('float',{
+        default: 0
+    })
+    price: number;
+
+    @Column({
+        type: 'text',
+        nullable: true,
+    })
+    description: string;
+
+    @Column('text',{
+        unique: true,
+    })
+    slug: string;
+
+    @Column('int',{
+        default: 0,
+    })
+    stock: number;
+
+    @Column('text',{
+        array: true,
+    })
+    sizes: string[];
+
+    @Column('text')
+    gender: string;
+
+    @Column('text',{
+        array: true,
+        default: []
+    })
+    tags: string[];
+
+
+    @BeforeInsert()
+    checkSlugInsert(){
+        if(!this.slug){
+            this.slug = this.title.toLocaleLowerCase().replaceAll(' ','_')
+        }
+
+        this.slug = this.slug.toLocaleLowerCase().replaceAll(' ','_')
+    }
+
+    @BeforeUpdate()
+    checkSlugUpdate(){
+        this.slug = this.slug.toLocaleLowerCase().replaceAll(' ','_')
+    }
+
 }

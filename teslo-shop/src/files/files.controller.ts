d09a341a88774df3b115ad71cef452496a5acb78
Marco Antonio Guadalupe Rivas Guzman <za19012397@zapopan.tecmm.edu.mx>
2025-07-24 17:44:51 -0600

    feat: Se agrego una configuracion para tomar la ruta del endpoint desde una variable del .env, se agrego una ruta para poder servir una imagen desde el endpoint

--- teslo-shop/src/files/files.controller.ts
+++ teslo-shop/src/files/files.controller.ts
@@ -1,18 +1,33 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException, Res } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FileFilter } from './helpers/fileFilter.helper';
 import { diskStorage } from 'multer';
+import { fileNamer } from './helpers/fileNamer.helper';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Get('product/:imageName')
+  findProductImage(@Res() res: Response, @Param('imageName') imageName: string){
+
+    const path = this.filesService.getStaticProductImage(imageName)
+
+    res.sendFile(path)
+  }
 
   @Post('product')
   @UseInterceptors(FileInterceptor('file', {fileFilter: FileFilter,
     storage: diskStorage({
-      destination: './static/uploads'
+      destination: './static/products',
+      filename: fileNamer,
     })
   }))
   uploadFile(@UploadedFile() file: Express.Multer.File,){
@@ -21,6 +36,10 @@ export class FilesController {
       throw new BadRequestException('Make sure that the file is an image');
     }
 
-    return file;
+    const secureUlr = `${this.configService.get('HOST_API')}/files/product/${file.filename}`;
+
+    return {
+      secureUlr
+    };
   }
 }

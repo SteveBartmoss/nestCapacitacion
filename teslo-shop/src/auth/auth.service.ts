c6d87dd84e65f165e63fa8122be2ad8449ad0fa6
Marco Antonio Guadalupe Rivas Guzman <za19012397@zapopan.tecmm.edu.mx>
2025-07-30 17:22:14 -0600

    feat: Se implemento la funcion para el login, se implemento una funcion para generar un token, se implemento el regreso del token en la funcion para crear y para logear un usuario

--- teslo-shop/src/auth/auth.service.ts
+++ teslo-shop/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -7,14 +7,18 @@ import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import * as bcrypt from 'bcrypt';
 import { LoginUserDto } from './dto/login-user.dto';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
   constructor(
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>
+    private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
   ){}
+
   async create(createUserDto: CreateUserDto) {
 
     try{
@@ -27,12 +31,36 @@ export class AuthService {
 
       await this.userRepository.save(user)
 
-      return userData;
+      return {
+        ...user,
+        token: this.getJwtToken({email: user.email})
+      }
     
     }catch(error){
       this.handleDBErrors(error)
     }
 
+  }
+
+  async login(loginUserDto: LoginUserDto){
+    const {password, email} = loginUserDto
+
+    const user = await this.userRepository.findOne({
+      where: {email},
+      select: {email: true, password: true}
+    });
+
+    if(!user){
+      throw new UnauthorizedException('Credentials are not valid (email)')
+    }
+
+    if(!bcrypt.compareSync(password, user.password)){
+      throw new UnauthorizedException('Credentials are not valid (password)')
+    }
+
+    return {...user,
+      token: this.getJwtToken({email: user.email})
+    };
   }
 
   findAll() {
@@ -51,17 +79,9 @@ export class AuthService {
     return `This action removes a #${id} auth`;
   }
 
-  async login(loginUserDto: LoginUserDto){
-    try{
-      const {password, email} = loginUserDto
-
-      const user = await this.userRepository.findOneBy({email})
-
-      return user
-
-    }catch(error){
-      this.handleDBErrors(error)
-    }
+  private getJwtToken(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
+    return token;
   }
 
   private handleDBErrors(error: any){
